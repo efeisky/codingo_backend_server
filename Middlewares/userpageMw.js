2385015c92dcdd554fb5d2a7709c2568b81fd509
Efeisky <59496053+efeisky@users.noreply.github.com>
2023-05-21 10:54:23 +0300

    Update userpageMw.js

--- Middlewares/userpageMw.js
+++ Middlewares/userpageMw.js
@@ -196,8 +196,8 @@ module.exports = (app) => {
     app.delete('/deleteAccount',bodyParser.json(),async (req,res)=>{
         const {name} = req.query;
         console.log(name)
-        //const reqData = await processDB.deleteAccount(name);
-        
+        const reqData = await processDB.deleteAccount(name);
+        console.log(reqData)
         res.send({
             deleteStatus : 1
         })
