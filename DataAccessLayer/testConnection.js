fc882ab21936f7fe8ab367c43e333bec04f68500
Efeisky <59496053+efeisky@users.noreply.github.com>
2024-05-20 22:21:32 +0300

    error

--- DataAccessLayer/testConnection.js
+++ DataAccessLayer/testConnection.js
@@ -1,14 +1,15 @@
-const pool = require('./connection')
+const pool = require('./connection');
 
 module.exports.testConnect = async function () {
     return new Promise((resolve, reject) => {
         pool.getConnection((err, connection) => {
-          if (err) {
-            reject({ dbStatus: 0 });
-          }
-          connection.release();
-          resolve({ dbStatus: 1 });
+            if (err) {
+                console.error("Error getting connection from pool:", err);
+                reject({ dbStatus: 0 });
+                return;
+            }
+            connection.release();
+            resolve({ dbStatus: 1 });
         });
-      });
-      
+    });
 }
